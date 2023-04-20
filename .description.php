19c2ac63b55d03d25444d5dcc4f4a1a7596f4538
Konstantin <kvt@iplogic.ru>
2023-04-20 13:24:04 +0300

    Update 20.04.23

--- .description.php
+++ .description.php
@@ -2,8 +2,8 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
 $arComponentDescription = array(
-	"NAME" => GetMessage("CSORT_COMPONENT_NAME"),
-	"DESCRIPTION" => GetMessage("CSORT_COMPONENT_DESCRIPTION"),
+	"NAME" => GetMessage("IPL_CSORT_COMPONENT_NAME"),
+	"DESCRIPTION" => GetMessage("IPL_CSORT_COMPONENT_DESCRIPTION"),
 	"ICON" => "/images/cat_detail.gif",
 	"CACHE_PATH" => "Y",
 	"PATH" => array (
--- lang/ru/.description.php
+++ lang/ru/.description.php
@@ -1,4 +1,4 @@
 <?
-$MESS ['CSORT_COMPONENT_NAME'] = 'Выбор порядка сортировки';
-$MESS ['CSORT_COMPONENT_DESCRIPTION'] = 'Выбор пользователем порядка сортировки товаров в списке';
+$MESS ['IPL_CSORT_COMPONENT_NAME'] = 'Выбор порядка сортировки';
+$MESS ['IPL_CSORT_COMPONENT_DESCRIPTION'] = 'Выбор пользователем порядка сортировки товаров в списке';
 ?>
